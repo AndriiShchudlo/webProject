aaa6643576d08675db11749c4b37b33e91560049
ShAndriy <andriyshchudlo@gmail.com>
2016-12-21 16:14:51 +0200

    create dpring MVC

--- src/main/java/ua/controller/myControler.java
+++ src/main/java/ua/controller/myControler.java
@@ -1,7 +1,20 @@
 package ua.controller;
 
-/**
- * Created by andriis on 12/21/16.
- */
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
 public class myControler {
+
+
+    @RequestMapping(value="/index", method = RequestMethod.GET)
+
+    public String Hello() {
+
+        return "index";
+    }
 }
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: andriis
+  Date: 12/21/16
+  Time: 3:02 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<p><h3> <a href="?path=/home"> "file structure"</a></h3></p>
+
+<h1> Hello World</h1>
+</body>
+</html>
